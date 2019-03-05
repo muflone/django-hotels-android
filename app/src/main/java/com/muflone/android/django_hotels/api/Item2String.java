b2af076672d14b4cef6f92c19675a73405d92ff6
Muflone <webreg@vbsimple.net>
2019-03-06 00:37:34 +0100

    Generic class Item2String with id and value fields

---
+++ app/src/main/java/com/muflone/android/django_hotels/api/Item2String.java
@@ -0,0 +1,20 @@
+package com.muflone.android.django_hotels.api;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Item2String {
+    public final String id;
+    public final String value;
+
+    public Item2String(JSONObject jsonObject, String idName, String valueName) throws JSONException {
+        // Generic constructor with field names
+        this.id = jsonObject.getString(idName);
+        this.value = jsonObject.getString(valueName);
+    }
+
+    public Item2String(JSONObject jsonObject) throws JSONException {
+        // Default constructor with implicit id and name fields
+        this(jsonObject, "id", "name");
+    }
+}
