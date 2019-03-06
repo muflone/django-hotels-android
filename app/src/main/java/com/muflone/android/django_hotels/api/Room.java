eaa6e74f650c533038727326a48f20cc983eb0ba
Muflone <webreg@vbsimple.net>
2019-03-06 01:28:08 +0100

    New class Room

---
+++ app/src/main/java/com/muflone/android/django_hotels/api/Room.java
@@ -0,0 +1,15 @@
+package com.muflone.android.django_hotels.api;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Room extends Item2 {
+    public final String roomType;
+    public final String bedType;
+
+    public Room(JSONObject jsonObject) throws JSONException {
+        super(jsonObject.getJSONObject("room"), "id", "name");
+        this.roomType = jsonObject.getString("room_type");
+        this.bedType = jsonObject.getString("bed_type");
+    }
+}
