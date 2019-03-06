1437b5f9447512958892441d35291af8a51c952c
Muflone <webreg@vbsimple.net>
2019-03-06 01:29:20 +0100

    New class Building

---
+++ app/src/main/java/com/muflone/android/django_hotels/api/Building.java
@@ -0,0 +1,25 @@
+package com.muflone.android.django_hotels.api;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Building extends Item2 {
+    public final Location location;
+    public final List<Room> rooms;
+
+    public Building(JSONObject jsonObject) throws JSONException {
+        super(jsonObject.getJSONObject("building"), "id", "name");
+        location = new Location(jsonObject.getJSONObject("location"));
+        JSONArray jsonRooms = jsonObject.getJSONArray("rooms");
+        // Loop over every room
+        this.rooms = new ArrayList<Room>();
+        for (int i = 0; i < jsonRooms.length(); i++) {
+            Room room = new Room(jsonRooms.getJSONObject(i));
+            this.rooms.add(room);
+        }
+    }
+}
