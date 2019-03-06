24770623094b66a8ca46c9665fdb7eb55a571303
Muflone <webreg@vbsimple.net>
2019-03-06 01:29:46 +0100

    New class Structure

---
+++ app/src/main/java/com/muflone/android/django_hotels/api/Structure.java
@@ -0,0 +1,33 @@
+package com.muflone.android.django_hotels.api;
+
+import android.os.Build;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Structure {
+    private final Item2 structure;
+    private final Item2 company;
+    private final Item2 brand;
+    private final Location location;
+    private final List<Building> buildings;
+
+    public Structure(JSONObject jsonObject) throws JSONException {
+        structure = new Item2(jsonObject.getJSONObject("structure"));
+        company = new Item2(jsonObject.getJSONObject("company"));
+        brand = new Item2(jsonObject.getJSONObject("brand"));
+        location = new Location(jsonObject.getJSONObject("location"));
+
+        // Loop over every building
+        JSONArray jsonBuildings = jsonObject.getJSONArray("buildings");
+        this.buildings = new ArrayList<Building>();
+        for (int i = 0; i < jsonBuildings.length(); i++) {
+            Building building = new Building(jsonBuildings.getJSONObject(i));
+            this.buildings.add(building);
+        }
+    }
+}
