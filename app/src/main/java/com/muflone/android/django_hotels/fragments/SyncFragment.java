46f177e371d7a519439121e082b5696253c5270a
Muflone <webreg@vbsimple.net>
2019-03-06 01:31:19 +0100

    Check on complete request from getData

--- app/src/main/java/com/muflone/android/django_hotels/fragments/SyncFragment.java
+++ app/src/main/java/com/muflone/android/django_hotels/fragments/SyncFragment.java
@@ -21,6 +21,10 @@ public class SyncFragment extends Fragment {
             Toast.makeText(getActivity(), getString(R.string.message_unmatching_date_time), Toast.LENGTH_SHORT).show();
         }
         System.out.println(api.getCurrentTokenCode());
+        // Download data from the server
+        if (!api.getData()) {
+            Toast.makeText(getActivity(), getString(R.string.message_unable_to_download), Toast.LENGTH_SHORT).show();
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.sync_fragment, container, false);
     }
