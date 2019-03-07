cd6a8ddc8c574cd8cb02002b062b02b1b7bdf380
Muflone <webreg@vbsimple.net>
2019-03-07 23:40:34 +0100

    Generic class Item4 with id, value, value2 and value3 fields

---
+++ app/src/main/java/com/muflone/android/django_hotels/api/Item4.java
@@ -0,0 +1,21 @@
+package com.muflone.android.django_hotels.api;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Item4 extends Item3 {
+    public final String value3;
+
+    public Item4(JSONObject jsonObject, String idName, String valueName, String value2Name, String value3Name) throws JSONException {
+        // Generic constructor with field names
+        super(jsonObject, idName, valueName, value2Name);
+        this.value3 = jsonObject.getString(value3Name);
+    }
+
+    public Item4(JSONObject jsonObject, String value2Name, String value3Name) throws JSONException {
+        // Default constructor with implicit id and name fields
+        super(jsonObject, value2Name);
+        this.value3 = jsonObject.getString(value3Name);
+    }
+
+}
