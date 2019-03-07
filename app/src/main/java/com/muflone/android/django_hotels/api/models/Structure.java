bfb18429b587c41648774e7f25f229e7dc2d9c4d
Muflone <webreg@vbsimple.net>
2019-03-08 00:04:07 +0100

    Subclassed Structure derived from Item2

--- app/src/main/java/com/muflone/android/django_hotels/api/models/Structure.java
+++ app/src/main/java/com/muflone/android/django_hotels/api/models/Structure.java
@@ -9,15 +9,14 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Structure {
-    private final Item2 structure;
+public class Structure extends Item2 {
     private final Company company;
     private final Brand brand;
     private final Location location;
     private final List<Building> buildings;
 
     public Structure(JSONObject jsonObject) throws JSONException {
-        structure = new Item2(jsonObject.getJSONObject("structure"));
+        super(jsonObject.getJSONObject("structure"), "id", "name");
         company = new Company(jsonObject.getJSONObject("company"));
         brand = new Brand(jsonObject.getJSONObject("brand"));
         location = new Location(jsonObject.getJSONObject("location"));
