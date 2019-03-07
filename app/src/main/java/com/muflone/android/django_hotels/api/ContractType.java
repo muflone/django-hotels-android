c0d18cfd3eb7401c0b1a35ad48a884c9f8bd8969
Muflone <webreg@vbsimple.net>
2019-03-07 23:41:07 +0100

    New class ContractType

---
+++ app/src/main/java/com/muflone/android/django_hotels/api/ContractType.java
@@ -0,0 +1,15 @@
+package com.muflone.android.django_hotels.api;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ContractType extends Item2 {
+    public final int dailyHours;
+    public final int weeklyHours;
+
+    public ContractType(JSONObject jsonObject) throws JSONException {
+        super(jsonObject, "id", "name");
+        this.dailyHours = jsonObject.getInt("daily");
+        this.weeklyHours = jsonObject.getInt("weekly");
+    }
+}
