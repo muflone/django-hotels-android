89faeec7080ee02d2c46f5420c11e6d692d30601
Muflone <webreg@vbsimple.net>
2019-03-11 20:49:51 +0100

    New class AppDatabase

---
+++ app/src/main/java/com/muflone/android/django_hotels/database/AppDatabase.java
@@ -0,0 +1,31 @@
+package com.muflone.android.django_hotels.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.muflone.android.django_hotels.database.dao.BrandDao;
+import com.muflone.android.django_hotels.database.models.Brand;
+
+@Database(entities = {Brand.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static final String DATABASE_NAME = "hotels.sqlite";
+    private static AppDatabase INSTANCE;
+    public abstract BrandDao brandDao();
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DATABASE_NAME)
+                // allow queries on the main thread.
+                // Don’t do this on a real app! See PersistenceBasicSample for an example.
+                // .allowMainThreadQueries()
+                .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
