727c2f400c4787d7b216f2dcf2ecf7315bd14a31
Muflone <webreg@vbsimple.net>
2019-03-17 18:23:55 +0100

    New Singleton class

---
+++ app/src/main/java/com/muflone/android/django_hotels/Singleton.java
@@ -0,0 +1,35 @@
+package com.muflone.android.django_hotels;
+
+import android.content.Context;
+
+import com.muflone.android.django_hotels.api.Api;
+
+import java.io.Serializable;
+
+public class Singleton implements Serializable {
+    private static volatile Singleton instance;
+
+    private Singleton() {
+        // Prevent form the reflection api.
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // Make singleton from serialize and deserialize operation.
+    protected Singleton readResolve() {
+        return getInstance();
+    }
+}
