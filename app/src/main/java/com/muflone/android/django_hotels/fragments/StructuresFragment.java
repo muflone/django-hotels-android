d93d228161e8e99935c056628d997739c6716450
Muflone <webreg@vbsimple.net>
2019-03-20 23:18:04 +0100

    Added second version with three columns

--- app/src/main/java/com/muflone/android/django_hotels/fragments/StructuresFragment.java
+++ app/src/main/java/com/muflone/android/django_hotels/fragments/StructuresFragment.java
@@ -1,6 +1,5 @@
 package com.muflone.android.django_hotels.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,9 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.muflone.android.django_hotels.R;
 import com.muflone.android.django_hotels.Singleton;
@@ -24,12 +21,19 @@ import com.muflone.android.django_hotels.database.models.Room;
 import com.muflone.android.django_hotels.database.models.Structure;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StructuresFragment extends Fragment {
     private View rootLayout;
     private TabLayout structuresTabs;
-    private TabLayout buildingsTabs;
+    private ListView employeesView;
+    private List<String> employeesList = new ArrayList<>();
+
+    private ListView buildingsView;
+    private List<String> buildingsList = new ArrayList<>();
+
     private ListView roomsView;
+    private List<String> roomsList = new ArrayList<>();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -38,8 +42,28 @@ public class StructuresFragment extends Fragment {
         ApiData apiData = Singleton.getInstance().apiData;
         // Inflate the layout for this fragment
         this.rootLayout = inflater.inflate(R.layout.structures_fragment, container, false);
-        // Initialize structuresTabs
-        this.structuresTabs = this.rootLayout.findViewById(R.id.structuresLayout);
+        this.structuresTabs = rootLayout.findViewById(R.id.structuresLayout);
+
+        this.employeesView = rootLayout.findViewById(R.id.employeesView);
+        this.employeesView.setAdapter(new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, this.employeesList));
+
+        this.buildingsView = rootLayout.findViewById(R.id.buildingsView);
+        this.buildingsView.setAdapter(new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, this.buildingsList));
+        this.buildingsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Log.i("testy", "I Clicked on Row " + position + " and it worked!");
+                view.setSelected(true);
+            }
+        });
+
+        this.roomsView = rootLayout.findViewById(R.id.roomsView);
+        this.roomsView.setAdapter(new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, this.roomsList));
+
+        // Initialize Structures
         this.structuresTabs.removeAllTabs();
         for (Structure structure : apiData.structures) {
             TabLayout.Tab tabItem = this.structuresTabs.newTab();
@@ -49,7 +73,7 @@ public class StructuresFragment extends Fragment {
         this.structuresTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                loadBuildingsTabs(tab);
+                loadBuildings(tab);
             }
 
             @Override
@@ -60,64 +84,42 @@ public class StructuresFragment extends Fragment {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
-        // Initialize buildingsTabs
-        this.buildingsTabs = rootLayout.findViewById(R.id.buildingsLayout);
-        this.buildingsTabs.removeAllTabs();
-        this.buildingsTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                loadRooms(tab);
-            }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
         // Select the first structure tab
-        this.roomsView = rootLayout.findViewById(R.id.roomsView);
-        this.loadBuildingsTabs(this.structuresTabs.getTabAt(0));
+        this.loadBuildings(this.structuresTabs.getTabAt(0));
+        this.buildingsView.requestFocusFromTouch();
+        this.buildingsView.setSelection(0);
+
         return rootLayout;
     }
 
-    protected void loadBuildingsTabs(TabLayout.Tab tab) {
+    protected void loadBuildings(TabLayout.Tab tab) {
         // Load buildings tabs for the selected Structure tab
+        buildingsList.clear();
         ApiData apiData = Singleton.getInstance().apiData;
-        this.buildingsTabs.removeAllTabs();
         int position = this.structuresTabs.getSelectedTabPosition();
         if (position >= 0) {
             Structure selectedStructure = apiData.structures.get(position);
             for (Building building : selectedStructure.buildings) {
-                TabLayout.Tab tabItem = this.buildingsTabs.newTab();
-                tabItem.setText(building.name.replace(
+                buildingsList.add(building.name.replace(
                         String.format("%s - ", selectedStructure.name),
                         "").replace(
                         String.format("%s ", selectedStructure.name),
                         ""));
-                this.buildingsTabs.addTab(tabItem);
             }
         }
+        // Update data
+        ((ArrayAdapter) buildingsView.getAdapter()).notifyDataSetChanged();
     }
 
     protected void loadRooms(TabLayout.Tab tab) {
         // Load rooms for the selected Building tab
         /*
-        ArrayList<Room> data = new ArrayList(
-                Singleton.getInstance().apiData.structures
-                        .get(structuresTabs.getSelectedTabPosition()).buildings
-                        .get(buildingsTabs.getSelectedTabPosition()).rooms);
-        this.roomsView.setAdapter(new CustomAdapter(data, getActivity()));
-        */
         ArrayList<String> data = new ArrayList();
-        for (Room room : Singleton.getInstance().apiData.structures
-                .get(structuresTabs.getSelectedTabPosition()).buildings
-                .get(buildingsTabs.getSelectedTabPosition()).rooms) {
+        for (Room room : this.roomsList.get(tab.getPosition())) {
             data.add(room.name);
         }
-        this.roomsView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data));
+        */
         this.roomsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
@@ -125,43 +127,4 @@ public class StructuresFragment extends Fragment {
             }
         });
     }
-/*
-    public class CustomAdapter extends ArrayAdapter<Room> {
-        private ArrayList<Room> dataSet;
-        private ViewHolder viewHolder;
-        private LayoutInflater inflater;
-
-        // View lookup cache
-        private class ViewHolder {
-            TextView textRoomName;
-            Button buttonSetWorker;
-        }
-
-        public CustomAdapter(ArrayList<Room> data, Context context) {
-            super(context, R.layout.structures_rooms_content, data);
-            this.dataSet = data;
-            this.inflater = LayoutInflater.from(context);
-            this.viewHolder = new ViewHolder();
-        }
-
-        @Override
-        public View getView(int position, View convertView, final ViewGroup parent) {
-            // Get the data item for this position
-            final Room room = getItem(position);
-            convertView = this.inflater.inflate(R.layout.structures_rooms_content,
-                    parent, false);
-            this.viewHolder.textRoomName = convertView.findViewById(R.id.textRoomName);
-            this.viewHolder.textRoomName.setText(room.name);
-            // this.viewHolder.buttonSetWorker = convertView.findViewById(R.id.buttonSetWorker);
-            // this.viewHolder.buttonSetWorker.setText(String.valueOf(room.id));
-            convertView.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Log.d("AA", "string: " + room.name);
-                    Log.d("AA", "int: " + room.id);
-                }
-            });
-            return convertView;
-        }
-    }
-*/
 }
