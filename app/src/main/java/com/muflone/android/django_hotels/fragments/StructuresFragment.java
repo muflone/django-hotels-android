4a8c7f2b3b3ce19ea88e0d83874ac99feb25c00d
Muflone <webreg@vbsimple.net>
2019-03-21 02:17:20 +0100

    Third version with CardView

--- app/src/main/java/com/muflone/android/django_hotels/fragments/StructuresFragment.java
+++ app/src/main/java/com/muflone/android/django_hotels/fragments/StructuresFragment.java
@@ -5,19 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.muflone.android.django_hotels.R;
 import com.muflone.android.django_hotels.Singleton;
 import com.muflone.android.django_hotels.api.ApiData;
-import com.muflone.android.django_hotels.database.models.Building;
-import com.muflone.android.django_hotels.database.models.Room;
+import com.muflone.android.django_hotels.database.models.Employee;
 import com.muflone.android.django_hotels.database.models.Structure;
 
 import java.util.ArrayList;
@@ -25,45 +24,31 @@ import java.util.List;
 
 public class StructuresFragment extends Fragment {
     private View rootLayout;
+    private Structure selectedStructure;
     private TabLayout structuresTabs;
     private ListView employeesView;
     private List<String> employeesList = new ArrayList<>();
 
-    private ListView buildingsView;
-    private List<String> buildingsList = new ArrayList<>();
-
-    private ListView roomsView;
-    private List<String> roomsList = new ArrayList<>();
+    private TextView firstNameView;
+    private TextView lastNameView;
+    private TextView genderView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        ApiData apiData = Singleton.getInstance().apiData;
-        // Inflate the layout for this fragment
-        this.rootLayout = inflater.inflate(R.layout.structures_fragment, container, false);
-        this.structuresTabs = rootLayout.findViewById(R.id.structuresLayout);
-
-        this.employeesView = rootLayout.findViewById(R.id.employeesView);
+        this.loadUI(inflater, container);
         this.employeesView.setAdapter(new ArrayAdapter<String>(
                 getActivity(), android.R.layout.simple_list_item_1, this.employeesList));
-
-        this.buildingsView = rootLayout.findViewById(R.id.buildingsView);
-        this.buildingsView.setAdapter(new ArrayAdapter<String>(
-                getActivity(), android.R.layout.simple_list_item_1, this.buildingsList));
-        this.buildingsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Log.i("testy", "I Clicked on Row " + position + " and it worked!");
-                view.setSelected(true);
+        this.employeesView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                loadEmployee(selectedStructure.employees.get(position));
             }
         });
 
-        this.roomsView = rootLayout.findViewById(R.id.roomsView);
-        this.roomsView.setAdapter(new ArrayAdapter<String>(
-                getActivity(), android.R.layout.simple_list_item_1, this.roomsList));
 
         // Initialize Structures
+        final ApiData apiData = Singleton.getInstance().apiData;
         this.structuresTabs.removeAllTabs();
         for (Structure structure : apiData.structures) {
             TabLayout.Tab tabItem = this.structuresTabs.newTab();
@@ -73,7 +58,13 @@ public class StructuresFragment extends Fragment {
         this.structuresTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                loadBuildings(tab);
+                loadEmployees(tab);
+                // Select the first employee for the selected tab
+                if (employeesList.size() > 0) {
+                    employeesView.requestFocusFromTouch();
+                    employeesView.setSelection(0);
+                    loadEmployee(selectedStructure.employees.get(0));
+                }
             }
 
             @Override
@@ -84,47 +75,45 @@ public class StructuresFragment extends Fragment {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
-
         // Select the first structure tab
-        this.loadBuildings(this.structuresTabs.getTabAt(0));
-        this.buildingsView.requestFocusFromTouch();
-        this.buildingsView.setSelection(0);
-
+        if (employeesList.size() > 0) {
+            this.structuresTabs.post(new Runnable() {
+                @Override
+                public void run() {
+                    loadEmployee(selectedStructure.employees.get(0));
+                }
+            });
+        }
         return rootLayout;
     }
 
-    protected void loadBuildings(TabLayout.Tab tab) {
-        // Load buildings tabs for the selected Structure tab
-        buildingsList.clear();
-        ApiData apiData = Singleton.getInstance().apiData;
-        int position = this.structuresTabs.getSelectedTabPosition();
-        if (position >= 0) {
-            Structure selectedStructure = apiData.structures.get(position);
-            for (Building building : selectedStructure.buildings) {
-                buildingsList.add(building.name.replace(
-                        String.format("%s - ", selectedStructure.name),
-                        "").replace(
-                        String.format("%s ", selectedStructure.name),
-                        ""));
-            }
-        }
-        // Update data
-        ((ArrayAdapter) buildingsView.getAdapter()).notifyDataSetChanged();
+    protected void loadUI(@NonNull final LayoutInflater inflater, @NonNull final ViewGroup container) {
+        // Inflate the layout for this fragment
+        this.rootLayout = inflater.inflate(R.layout.structures_fragment, container, false);
+        // Save references
+        this.structuresTabs = rootLayout.findViewById(R.id.structuresLayout);
+        this.employeesView = rootLayout.findViewById(R.id.employeesView);
+        this.firstNameView = rootLayout.findViewById(R.id.firstNameView);
+        this.lastNameView = rootLayout.findViewById(R.id.lastNameView);
+        this.genderView = rootLayout.findViewById(R.id.genderView);
     }
 
-    protected void loadRooms(TabLayout.Tab tab) {
-        // Load rooms for the selected Building tab
-        /*
-        ArrayList<String> data = new ArrayList();
-        for (Room room : this.roomsList.get(tab.getPosition())) {
-            data.add(room.name);
+    protected void loadEmployees(TabLayout.Tab tab) {
+        // Load employess list for the selected Structure tab
+        employeesList.clear();
+        ApiData apiData = Singleton.getInstance().apiData;
+        this.selectedStructure = apiData.structures.get(tab.getPosition());
+        for (Employee employee : this.selectedStructure.employees) {
+            employeesList.add(String.format("%s %s", employee.firstName, employee.lastName));
         }
-        */
-        this.roomsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Log.i("testy", "I Clicked on Row " + position + " and it worked!");
-            }
-        });
+        // Update data
+        ((ArrayAdapter) employeesView.getAdapter()).notifyDataSetChanged();
+    }
+
+    protected void loadEmployee(Employee employee) {
+        // Load Employee details
+        this.firstNameView.setText(employee.firstName);
+        this.lastNameView.setText(employee.lastName);
+        this.genderView.setText(employee.gender);
     }
 }
