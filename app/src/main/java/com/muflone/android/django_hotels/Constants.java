67755fcd950ed82d648762a4c94c531bcf9489a6
Muflone <webreg@vbsimple.net>
2019-03-27 00:04:44 +0100

    Updated database version

--- app/src/main/java/com/muflone/android/django_hotels/Constants.java
+++ app/src/main/java/com/muflone/android/django_hotels/Constants.java
@@ -1,6 +1,6 @@
 package com.muflone.android.django_hotels;
 
 public class Constants {
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "hotels.sqlite";
 }
