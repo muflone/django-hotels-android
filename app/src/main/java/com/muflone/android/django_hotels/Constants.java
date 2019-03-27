49913413b4ae7023d2a722aa2da888ac4e9b875b
Muflone <webreg@vbsimple.net>
2019-03-27 23:37:49 +0100

    Database schema bump

--- app/src/main/java/com/muflone/android/django_hotels/Constants.java
+++ app/src/main/java/com/muflone/android/django_hotels/Constants.java
@@ -1,6 +1,6 @@
 package com.muflone.android.django_hotels;
 
 public class Constants {
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "hotels.sqlite";
 }
