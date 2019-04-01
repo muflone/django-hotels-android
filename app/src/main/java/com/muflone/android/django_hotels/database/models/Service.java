e11342d339a616536d1d10a687d1f3f60cd2670d
Muflone <webreg@vbsimple.net>
2019-04-01 20:42:14 +0200

    Added show_in_app field to Services

--- app/src/main/java/com/muflone/android/django_hotels/database/models/Service.java
+++ app/src/main/java/com/muflone/android/django_hotels/database/models/Service.java
@@ -18,15 +18,20 @@ public class Service {
     @ColumnInfo(name = "extra_service")
     public final boolean extra_service;
 
-    public Service(long id, String name, boolean extra_service) {
+    @ColumnInfo(name = "show_in_app")
+    public final boolean show_in_app;
+
+    public Service(long id, String name, boolean extra_service, boolean show_in_app) {
         this.id = id;
         this.name = name;
         this.extra_service = extra_service;
+        this.show_in_app = show_in_app;
     }
 
     public Service(JSONObject jsonObject) throws JSONException {
         this(jsonObject.getLong("id"),
                 jsonObject.getString("name"),
-                jsonObject.getBoolean("extra_service"));
+                jsonObject.getBoolean("extra_service"),
+                jsonObject.getBoolean("show_in_app"));
     }
 }
