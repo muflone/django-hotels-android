20fee9fb3c08020d099243276352fb92ca39bb87
Muflone <webreg@vbsimple.net>
2019-04-09 00:11:21 +0200

    Code cleanup

--- app/src/main/java/com/muflone/android/django_hotels/activities/MainActivity.java
+++ app/src/main/java/com/muflone/android/django_hotels/activities/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
