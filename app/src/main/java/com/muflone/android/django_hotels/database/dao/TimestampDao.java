739778384e4d0c501e7a91b2b4bbf83db14f5637
Muflone <webreg@vbsimple.net>
2019-05-19 16:56:27 +0200

    Filtered timestamps by selected structure

--- app/src/main/java/com/muflone/android/django_hotels/database/dao/TimestampDao.java
+++ app/src/main/java/com/muflone/android/django_hotels/database/dao/TimestampDao.java
@@ -20,7 +20,7 @@ public interface TimestampDao {
            "FROM timestamps")
     List<Timestamp> listAll();
 
-    @Query("SELECT " +
+    @Query("SELECT DISTINCT " +
            "  timestamps.id, " +
            "  employees.first_name, " +
            "  employees.last_name, " +
@@ -30,15 +30,20 @@ public interface TimestampDao {
            "FROM timestamps " +
            "INNER JOIN contracts " +
            "   ON contracts.id = timestamps.contract_id " +
+           "INNER JOIN contract_buildings " +
+           "   ON contract_buildings.contract_id = contracts.id " +
+           "INNER JOIN buildings " +
+           "   ON buildings.id = contract_buildings.building_id " +
            "INNER JOIN employees " +
            "   ON employees.id = contracts.employee_id " +
            "INNER JOIN timestamp_directions " +
            "   ON timestamp_directions.id = timestamps.direction_id " +
-           "WHERE datetime BETWEEN :date AND :date + 86400000 - 1 " +
+           "WHERE buildings.structure_id = :structureId " +
+           "  AND datetime BETWEEN :date AND :date + 86400000 - 1 " +
            "  AND (timestamp_directions.type_enter = 1 OR timestamp_directions.type_exit = 1) " +
            "ORDER BY timestamps.datetime DESC " +
            "LIMIT :count")
-    List<TimestampEmployee> listByLatestEnterExit(Date date, long count);
+    List<TimestampEmployee> listByLatestEnterExit(Date date, long structureId, long count);
 
     @Query("SELECT * " +
            "FROM timestamps " +
