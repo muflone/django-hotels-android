896f247904fd1e1e68ecd036c5f25b1fea937d6a
Muflone <webreg@vbsimple.net>
2019-06-09 20:09:00 +0200

    New CommandLog to print a message to the log

---
+++ app/src/main/java/com/muflone/android/django_hotels/commands/CommandLog.java
@@ -0,0 +1,60 @@
+package com.muflone.android.django_hotels.commands;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import com.muflone.android.django_hotels.database.models.Command;
+
+import org.json.JSONException;
+
+public class CommandLog extends CommandBase {
+    /**
+     * This Command prints a message to the log
+     *
+     * The command must have the following arguments:
+     * type: must be one of the following:
+     *   d for debug message
+     *   w for warning message
+     *   v for verbose message
+     *   i for information message
+     *   e for error message
+     *   a for abnormal message
+     * tag: the message tag
+     * message: the message to show
+     */
+    public CommandLog(Activity activity, Context context, Context applicationContext, Command command) {
+        super(activity, context, applicationContext, command);
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+        try {
+            String messageTag = this.command.type.command.getString("tag");
+            String messageText = this.command.type.command.getString("message");
+            switch (this.command.type.command.getString("type")) {
+                case "d":
+                    Log.d(messageTag, messageText);
+                    break;
+                case "w":
+                    Log.w(messageTag, messageText);
+                    break;
+                case "v":
+                    Log.v(messageTag, messageText);
+                    break;
+                case "i":
+                    Log.i(messageTag, messageText);
+                    break;
+                case "e":
+                    Log.e(messageTag, messageText);
+                    break;
+                case "a":
+                    Log.wtf(messageTag, messageText);
+                    break;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
