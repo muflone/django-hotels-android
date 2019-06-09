63d76a859fcf9ae40861be388643e5925aded71f
Muflone <webreg@vbsimple.net>
2019-06-09 17:53:35 +0200

    Added Command context constants

--- app/src/main/java/com/muflone/android/django_hotels/Constants.java
+++ app/src/main/java/com/muflone/android/django_hotels/Constants.java
@@ -4,4 +4,15 @@ public class Constants {
     public static final int DATABASE_VERSION = 7;
     public static final String DATABASE_NAME = "hotels.sqlite";
     public static final int LATEST_TIMESTAMPS = 30;
+
+    // Command Contexts for CommandFactory
+    public static final String CONTEXT_APP_BEGIN = "APP BEGIN";
+    public static final String CONTEXT_APP_END = "APP END";
+    public static final String CONTEXT_START_MAIN = "START MAIN";
+    public static final String CONTEXT_START_STRUCTURE = "START STRUCTURE";
+    public static final String CONTEXT_START_EXTRA = "START EXTRA";
+    public static final String CONTEXT_START_SYNC = "START SYNC";
+    public static final String CONTEXT_START_ABOUT = "START ABOUT";
+    public static final String CONTEXT_SYNC_BEGIN = "SYNC BEGIN";
+    public static final String CONTEXT_SYNC_END = "SYNC END";
 }
