bae29d96a4c4d4818f903b9b1f036d360b4f769a
Muflone <webreg@vbsimple.net>
2019-07-14 02:28:36 +0200

    Used the CommandSetUsed to set command used count

--- app/src/main/java/com/muflone/android/django_hotels/commands/CommandSetUsed.java
+++ app/src/main/java/com/muflone/android/django_hotels/commands/CommandSetUsed.java
@@ -2,11 +2,11 @@ package com.muflone.android.django_hotels.commands;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.AsyncTask;
 
 import com.muflone.android.django_hotels.Singleton;
 import com.muflone.android.django_hotels.database.models.Command;
 import com.muflone.android.django_hotels.database.models.CommandUsage;
+import com.muflone.android.django_hotels.tasks.TaskCommandSetUsed;
 
 import org.json.JSONException;
 
@@ -34,36 +34,18 @@ public class CommandSetUsed extends CommandBase {
             // Find CommandUsages to update
             if (commandId == CommandConstants.SET_USED_SET_ALL_COMMAND_USAGES) {
                 // Update every CommandUsages
-                new CommandUsedUpdateDatabaseTask(commandUsed).execute(commandUsages);
+                new TaskCommandSetUsed(commandUsed).execute(commandUsages);
             } else {
                 // Find the CommandUsage to update
                 for (CommandUsage commandUsage : commandUsages) {
                     if (commandUsage.id == commandId) {
-                        new CommandUsedUpdateDatabaseTask(commandUsed).execute(commandUsage);
+                        new TaskCommandSetUsed(commandUsed).execute(commandUsage);
                         break;
                     }
                 }
             }
         } catch (JSONException exception) {
             exception.printStackTrace();
-        }
-    }
-
-    private static class CommandUsedUpdateDatabaseTask extends AsyncTask<CommandUsage, Void, Void> {
-        private final Singleton singleton = Singleton.getInstance();
-        private final int used;
-
-        public CommandUsedUpdateDatabaseTask(int used) {
-            this.used = used;
-        }
-
-        @Override
-        protected Void doInBackground(CommandUsage... params) {
-            for (CommandUsage commandUsage : params) {
-                commandUsage.used = this.used;
-                this.singleton.database.commandUsageDao().update(commandUsage);
-            }
-            return null;
         }
     }
 }
