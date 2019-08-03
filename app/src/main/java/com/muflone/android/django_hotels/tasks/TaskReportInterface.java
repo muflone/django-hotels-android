1805b97d59c165532bfa341780964794aeadcc68
Muflone <webreg@vbsimple.net>
2019-08-03 20:18:30 +0200

    New interface TaskReportInterface

---
+++ app/src/main/java/com/muflone/android/django_hotels/tasks/TaskReportInterface.java
@@ -0,0 +1,9 @@
+package com.muflone.android.django_hotels.tasks;
+
+/*
+ * Listener for TaskReports
+ */
+public interface TaskReportInterface {
+    void showHTML(String data);
+    void createPDF(String data);
+}
