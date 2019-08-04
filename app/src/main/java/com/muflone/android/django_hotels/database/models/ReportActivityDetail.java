273edf9e61d1def70458d5634fe0e0381b3a2b77
Muflone <webreg@vbsimple.net>
2019-08-04 16:52:22 +0200

    Removed @NotNulls

--- app/src/main/java/com/muflone/android/django_hotels/database/models/ReportActivityDetail.java
+++ app/src/main/java/com/muflone/android/django_hotels/database/models/ReportActivityDetail.java
@@ -1,7 +1,6 @@
 package com.muflone.android.django_hotels.database.models;
 
 import android.arch.persistence.room.ColumnInfo;
-import android.support.annotation.NonNull;
 
 import java.util.Date;
 
@@ -10,49 +9,41 @@ public class ReportActivityDetail {
     public long companyId;
 
     @ColumnInfo(name = "company")
-    @NonNull
     public String company;
 
     @ColumnInfo(name = "employee_id")
     public long employeeId;
 
     @ColumnInfo(name = "first_name")
-    @NonNull
     public String firstName;
 
     @ColumnInfo(name = "last_name")
-    @NonNull
     public String lastName;
 
     @ColumnInfo(name = "contract_id")
     public long contractId;
 
     @ColumnInfo(name = "datetime")
-    @NonNull
     public Date datetime;
 
     @ColumnInfo(name = "building_id")
     public long buildingId;
 
     @ColumnInfo(name = "building")
-    @NonNull
     public String building;
 
     @ColumnInfo(name = "room_id")
     public long roomId;
 
     @ColumnInfo(name = "room")
-    @NonNull
     public String room;
 
     @ColumnInfo(name = "service_id")
     public long serviceId;
 
     @ColumnInfo(name = "service")
-    @NonNull
     public String service;
 
     @ColumnInfo(name = "description")
-    @NonNull
     public String description;
 }
