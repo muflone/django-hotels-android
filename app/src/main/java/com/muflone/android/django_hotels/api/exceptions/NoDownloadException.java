22770ea54b0b430895f1e3617773fd2f0a64ce17
Muflone <webreg@vbsimple.net>
2019-08-25 22:06:35 +0200

    Added copyright notices to all source files

--- app/src/main/java/com/muflone/android/django_hotels/api/exceptions/NoDownloadException.java
+++ app/src/main/java/com/muflone/android/django_hotels/api/exceptions/NoDownloadException.java
@@ -1,3 +1,26 @@
+/*
+ *     Project: Django Hotels Android
+ * Description: The Android client companion app for Django Hotels
+ *     Website: http://www.muflone.com/django-hotels-android/
+ *      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+ *   Copyright: 2018-2019 Fabio Castelli
+ *     License: GPL-3+
+ * Source code: https://github.com/muflone/django-hotels-android
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.muflone.android.django_hotels.api.exceptions;
 
 public class NoDownloadException extends Exception {
